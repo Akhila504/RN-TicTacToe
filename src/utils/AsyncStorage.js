b40814bdbf162db40a3ae0299113af97fd9c24d1
Akhila504 <akhila.ambekar@gmail.com>
2021-05-13 11:26:33 +0530

    first commit

---
+++ src/utils/AsyncStorage.js
@@ -0,0 +1,23 @@
+import {AsyncStorage} from '@react-native-async-storage/async-storage';
+
+const GAMES_PLAYED_KEY = '@ticTacToe:gamesPlayed';
+
+export async function getGamesPlayed() {
+  try {
+    return (await AsyncStorage.getItem(GAMES_PLAYED_KEY)) || 0;
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function addNewGamePlayed() {
+  try {
+    const currentGamesPlayed = await AsyncStorage.getItem(GAMES_PLAYED_KEY);
+    const newGamesPlayed = ((Number(currentGamesPlayed) || 0) + 1).toString();
+
+    console.log('new Games', newGamesPlayed);
+    await AsyncStorage.setItem(GAMES_PLAYED_KEY, newGamesPlayed);
+  } catch (error) {
+    return error;
+  }
+}
